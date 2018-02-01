dd332cca24efabc430f29fd24b7c7448ee10dea3
王萍 <wang.03.25@qq.com>
2018-02-01 22:36:24 +0800

    修改为jedis，去掉spring

--- src/main/java/com/wp/test/LockTest.java
+++ src/main/java/com/wp/test/LockTest.java
@@ -1,21 +1,26 @@
-package com.wp.lock;
+package com.wp.test;
+
+import com.wp.lock.RedisLock;
 
 public class LockTest {
     //不加锁
     static class Outputer {
-        //创建一个名为redisLock的RedisLock类型的锁
+        /**
+         * 创建一个名为redisLock的RedisLock类型的锁
+         */
         RedisLock redisLock = new RedisLock("redisLock");
 
         public void output(String name) {
-            //上锁
             redisLock.lock();
             try {
                 for (int i = 0; i < name.length(); i++) {
                     System.out.print(name.charAt(i));
+                    Thread.sleep(1000);
                 }
                 System.out.println();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             } finally {
-                //任何情况下都要释放锁
                 redisLock.unlock();
             }
         }
@@ -29,14 +34,14 @@ public class LockTest {
             public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     output.output("zhangsan");
                 }
             }
-        }).start();
+        }, "thread1").start();
 
         //线程2打印lingsi
         new Thread(new Runnable() {
@@ -44,28 +49,28 @@ public class LockTest {
             public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     output.output("lingsi");
                 }
             }
-        }).start();
+        }, "thread2").start();
 
-        //线程3打印wangwu
+        //线程3打印huangwu
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(3000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     output.output("huangwu");
                 }
             }
-        }).start();
+        }, "thread3").start();
     }
 }
