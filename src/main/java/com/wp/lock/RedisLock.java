7269e751f8f3e418641db098b65fdfdd094b88f0
王萍 <wang.03.25@qq.com>
2018-02-02 11:35:16 +0800

    完善RedisLock，实现tryLock的两个方法

--- src/main/java/com/wp/lock/RedisLock.java
+++ src/main/java/com/wp/lock/RedisLock.java
@@ -15,23 +15,18 @@ public class RedisLock implements Lock {
      */
     private static final String LOCKED = "LOCKED";
     /**
-     * 请求锁的超时时间(ms)
-     */
-    private static final long TIME_OUT = 30000;
-
-    /**
      * 锁的有效时间(s)
      */
     public static final int EXPIRE = 60;
     private String key;
     private volatile boolean isLocked = false;
+    private volatile Thread lockOwner = null;
 
     private ThreadLocal<Jedis> localJedis = new ThreadLocal<Jedis>() {
         @Override
         protected Jedis initialValue() {
             return RedisUtil.getJedis();
         }
-
     };
 
     public RedisLock(String key) {
@@ -40,24 +35,17 @@ public class RedisLock implements Lock {
 
     @Override
     public void lock() {
-        //系统当前时间，纳秒
-        long nowTime = System.nanoTime();
-        //请求锁超时时间，毫秒 .           毫秒与纳秒相差1000000
-        long timeout = TIME_OUT * 1000000;
         final Random r = new Random();
         try {
-            //不断循环向Master节点请求锁，当请求时间(System.nanoTime() - nanoTime)超过设定的超时时间则放弃请求锁
-            //这个可以防止一个客户端在某个宕掉的master节点上阻塞过长时间
-            //如果一个master节点不可用了，应该尽快尝试下一个master节点
-            while ((System.nanoTime() - nowTime) < timeout) {
+            while (true) {
                 //将锁作为key存储到redis缓存中，存储成功则获得锁
-                if (1==localJedis.get().setnx(key.getBytes(), LOCKED.getBytes())) {
+                if (1 == localJedis.get().setnx(key.getBytes(), LOCKED.getBytes())) {
                     //设置锁的有效期，也是锁的自动释放时间，也是一个客户端在其他客户端能抢占锁之前可以执行任务的时间
                     //可以防止因异常情况无法释放锁而造成死锁情况的发生
                     localJedis.get().expire(key, EXPIRE);
                     //写入成功才会设为true。
                     isLocked = true;
-                    //上锁成功结束请求
+                    lockOwner = Thread.currentThread();
                     break;
                 }
                 //获取锁失败时，应该在随机延时后进行重试，避免不同客户端同时重试导致谁都无法拿到锁的情况出现
@@ -70,34 +58,50 @@ public class RedisLock implements Lock {
 
     @Override
     public void unlock() {
-        //释放锁
-        //不管请求锁是否成功，只要已经上锁，客户端都会进行释放锁的操作
-        if (isLocked) {
+        //释放锁的条件，锁处于锁定状态且当前线程是拥有所得线程。
+        if (isLocked && Thread.currentThread().equals(lockOwner)) {
             localJedis.get().del(key);
         }
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public boolean tryLock() {
-        // TODO Auto-generated method stub
+        if (1 == localJedis.get().setnx(key.getBytes(), LOCKED.getBytes())) {
+            localJedis.get().expire(key, EXPIRE);
+            isLocked = true;
+            lockOwner = Thread.currentThread();
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        // TODO Auto-generated method stub
+        long nanos = unit.toNanos(time);
+        long nowTime = System.nanoTime();
+        final Random r = new Random();
+        try {
+            while ((System.nanoTime() - nowTime) < nanos) {
+                if (1 == localJedis.get().setnx(key.getBytes(), LOCKED.getBytes())) {
+                    localJedis.get().expire(key, EXPIRE);
+                    isLocked = true;
+                    lockOwner = Thread.currentThread();
+                    return true;
+                }
+                Thread.sleep(r.nextInt(5) + 5);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public Condition newCondition() {
-        // TODO Auto-generated method stub
         return null;
     }
 }
